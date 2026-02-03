ff235c01ed3bed0814bf1c0d17ed7c5a5ef36191
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2026-02-03 12:27:42 +0100

    Add localhost as allowed origin

--- backend/src/middlewares/cors.middleware.ts
+++ backend/src/middlewares/cors.middleware.ts
@@ -4,12 +4,17 @@ export const corsMiddleware = () => {
   const frontendHost = process.env.LOCAL_FRONTEND_HOST
   const frontendPort = process.env.LOCAL_FRONTEND_PORT
 
+  const allowedOrigins = [
+    `http://localhost:${frontendPort}`,
+    `http://${frontendHost}:${frontendPort}`,
+  ]
+
   if (!frontendHost || !frontendPort) {
     throw new Error('Frontend host/port not defined in environment variables')
   }
 
   return cors({
-    origin: `http://${frontendHost}:${frontendPort}`,
+    origin: allowedOrigins,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
